a1562a643ca56b279cde2b7c0f911f4d96f0ee80
colin <colin.williams91@gmail.com>
2023-05-22 09:51:54 -0500

    global http filter exception applied

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -51,13 +51,16 @@ export class CoffeesService {
   }
 
   async findOne(id: string) {
-    const coffee = this.coffeeRepository.findOne({
+    const coffee = await this.coffeeRepository.findOne({
       where: { id: +id },
       relations: { flavors: true },
     });
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
-      // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND); /* <-- above equivalent */
+      // throw new HttpException(
+      //   `Coffee #${id} not found`,
+      //   HttpStatus.NOT_FOUND,
+      // ); /* <-- above equivalent */
     }
     return coffee;
   }
