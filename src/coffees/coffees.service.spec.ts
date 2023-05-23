e770f26bc866341f9c9d977af367459e3a17a4df
colin <colin.williams91@gmail.com>
2023-05-23 13:02:12 -0500

    findOne method specific test success/failure paths

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { ConfigModule } from '@nestjs/config';
 import { CoffeesModule } from './coffees.module';
 import { DatabaseModule } from '../database/database.module';
+import { NotFoundException } from '@nestjs/common';
 
 /**
  * other available test lifecycle options: beforeAll(), afterEach(), afterAll()
@@ -23,8 +24,17 @@ import { DatabaseModule } from '../database/database.module';
 
 // RUN: `npm run test:watch -- coffees.service`
 
+/* below: generic fn to return mocked obj w/ all methods that repository class provides*/
+/* note: default type aliasing: `<T = any>` */
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
+
 describe('CoffeesService', () => {
   let service: CoffeesService;
+  let coffeeRepository: MockRepository;
   /* "setup phase" beforeEach() hook */
   beforeEach(async () => {
     /* instantiate test module to get a hold of our CoffeeService (Provider) */
@@ -33,17 +43,52 @@ describe('CoffeesService', () => {
       providers: [
         CoffeesService,
         { provide: DataSource, useValue: {} },
-        { provide: getRepositoryToken(Flavor), useValue: {} }, // 👈
-        { provide: getRepositoryToken(Coffee), useValue: {} }, // 👈
+        {
+          provide: getRepositoryToken(Flavor),
+          useValue: createMockRepository(),
+        },
+        {
+          provide: getRepositoryToken(Coffee),
+          useValue: createMockRepository(),
+        },
       ],
     }).compile();
     /* the above test Module and methods are now available to all tests in this `describe` block */
 
     service = module.get<CoffeesService>(CoffeesService); // **
+    coffeeRepository = module.get<MockRepository>(getRepositoryToken(Coffee));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    // below: test for the success path
+    describe('when coffee with ID exists', () => {
+      it('should return the coffee object', async () => {
+        const coffeeId = '1';
+        const expectedCoffee = {};
+
+        coffeeRepository.findOne.mockReturnValue(expectedCoffee);
+        const coffee = await service.findOne(coffeeId);
+        expect(coffee).toEqual(expectedCoffee);
+      });
+    }); // below: test for the failure path
+    describe('otherwise', () => {
+      it('should throw the "NotFoundException"', async () => {
+        const coffeeId = '1';
+        coffeeRepository.findOne.mockReturnValue(undefined);
+        /* use `try/catch` to capture exceptions that *will* occur here (failure test path) */
+        try {
+          await service.findOne(coffeeId);
+          expect(false).toBeTruthy(); // we should never hit this line
+        } catch (err) {
+          expect(err).toBeInstanceOf(NotFoundException);
+          expect(err.message).toEqual(`Coffee #${coffeeId} not found`);
+        }
+      });
+    });
   });
 });
 
